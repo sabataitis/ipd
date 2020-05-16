f512aee2e14f7a0d0af46287b6004519acdc0fc0
sabataitis <sabataitisvilius@gmail.com>
2020-05-17 00:14:12 +0300

    Updates

---
+++ precache-manifest.a8f5a7d26fc9607454e5a8fb0dde0150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e799724102dfbfff2df9f82365b4b0",
+    "url": "/ipd/index.html"
+  },
+  {
+    "revision": "6dc6ca0e083c7c6d60df",
+    "url": "/ipd/static/css/main.a3bac37a.chunk.css"
+  },
+  {
+    "revision": "f58d9e0d4b7800b6dd95",
+    "url": "/ipd/static/js/2.bf874e16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ipd/static/js/2.bf874e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc6ca0e083c7c6d60df",
+    "url": "/ipd/static/js/main.84e16134.chunk.js"
+  },
+  {
+    "revision": "6955a62670146aa578a3",
+    "url": "/ipd/static/js/runtime-main.acb190ad.js"
+  }
+]);
